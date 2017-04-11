fe07a2f11a8d8735741105edb245882ecdc084c8
Philipp Bergt <philipp.bergt@st.ovgu.de>
2017-04-12 00:41:44 +0200

    added some simple classes + pom

---
+++ src/main/java/Truck.java
@@ -0,0 +1,5 @@
+/**
+ * Created by phillipbergt on 11.04.17.
+ */
+public class Truck extends Vehicle {
+}
---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,5 @@
+/**
+ * Created by phillipbergt on 11.04.17.
+ */
+public class Vehicle {
+}
